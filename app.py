5763b46cb57a32e8d71601440308ecd7701e8bdc
Kartik Agrawal <70594964+Kartik-beep@users.noreply.github.com>
2023-02-05 10:05:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from pytube import YouTube
 import urllib.request
 import re
 import shutil
+from moviepy.editor import *
 
 html = '''<!doctype html>
     <title>YT To MP3</title>
@@ -37,6 +38,10 @@ if os.path.exists('songs.zip'):
     os.remove('./songs.zip')
 else:
     pass
+def MP4ToMP3(mp4, mp3):
+    FILETOCONVERT = AudioFileClip(mp4)
+    FILETOCONVERT.write_audiofile(mp3)
+    FILETOCONVERT.close()
 
 def remove_line():
     with open('songs.txt', 'r') as fr:
@@ -48,6 +53,13 @@ def remove_line():
                     fw.write(line)
                 ptr += 1
 
+def check_link(link):
+    a = list(link)
+    if a[0] == 'h' and a[1] == 't' and a[2] == 't' and a[3] == 'p' and a[4] == 's':
+        return True
+    else:
+        return False
+
 def Download_Song(Name):
     destination = 'Songs'
     if os.path.exists('Songs'):
@@ -55,23 +67,27 @@ def Download_Song(Name):
     else:
         pass
     song_name = Name
-    print(song_name)
     for k in song_name.split("\n"):
-        song_line = re.sub(r"[^a-zA-Z0-9]+", ' ', k)
-        if ' ' in song_line:
-            song_artist = song_line.replace(' ', '+')
+        song_artist = k
+        if check_link(song_artist) == True:
+            yt_link = song_artist
         else:
-            song_artist = song_line
-        search_keyword = (song_artist + '+lyrics')
-        html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
-        video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-        yt_link = ("https://www.youtube.com/watch?v=" + video_ids[0])
+            song_line = re.sub(r"[^a-zA-Z0-9]+", ' ', k)
+            if ' ' in song_line:
+                song_artist = song_line.replace(' ', '+')
+            else:
+                song_artist = song_line
+            search_keyword = (song_artist + '+lyrics+audio')
+            html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
+            video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+            yt_link = ("https://www.youtube.com/watch?v=" + video_ids[0])
         yt = YouTube(str(yt_link))
         video = yt.streams.filter(only_audio=True).first()
         out_file = video.download(output_path=destination)
         base, ext = os.path.splitext(out_file)
-        new_file = base + '.mp3'
+        new_file = base + '.mp4'
         os.rename(out_file, new_file)
+        MP4ToMP3(new_file, base + '.mp3')
 
 def Download_File(filename):
     destination = 'downloads'
@@ -81,34 +97,38 @@ def Download_File(filename):
     for line in read_lines:
         count += 1
         try:
-            print('Downloading....')
             song_formatted = ("{}".format(line.strip()))
             for k in song_formatted.split("\n"):
-                song_line = re.sub(r"[^a-zA-Z0-9]+", ' ', k)
-                song = song_line.replace(' ', '+')
-                search_keyword=(song + '+lyrics')
-                html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
-                video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-                yt_link = ("https://www.youtube.com/watch?v=" + video_ids[0])
+                song = k
+                if check_link(song) == True:
+                    yt_link = song
+                else:
+                    song_line = re.sub(r"[^a-zA-Z0-9]+", ' ', k)
+                    song = song_line.replace(' ', '+')
+                    search_keyword = (song + '+lyrics+audio')
+                    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search_keyword)
+                    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+                    yt_link = ("https://www.youtube.com/watch?v=" + video_ids[0])
                 yt = YouTube(str(yt_link))
                 video = yt.streams.filter(only_audio=True).first()
                 out_file = video.download(output_path=destination)
                 base, ext = os.path.splitext(out_file)
-                new_file = base + '.mp3'
+                new_file = base + '.mp4'
                 os.rename(out_file, new_file)
+                MP4ToMP3(new_file, base + '.mp3')
+                os.remove(new_file)
                 try:
                     remove_line()
                 except:
-                    print(f"Couldn't remove {song_line}")		
-        except Exception:
+                    print(f"Couldn't remove {k}")		
+        except Exception as e:
+            print(e)
             error_file = open('error.txt', 'a')
-            error = '{}: {} '.format(count, song_line)
+            error = '{}: {} '.format(count, k)
             error_file.write(f'{error} \n')
-    print('Zipping...')
+    song_file.close()
     shutil.make_archive('songs', 'zip', './downloads/')
-    print('Clearing Downloads...')
     shutil.rmtree('./downloads/')
-    print('Removing File...')
     os.remove(filename)
 
 DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/Songs/'
@@ -132,7 +152,6 @@ def index():
     if request.method == 'POST':
         song = request.form['name']
         file = request.files['file']
-        print(file)
         if request.form['name'].isspace() == False and song != '':
             Download_Song(song)
             list_of_files = glob.glob('./Songs/*.mp3')
@@ -147,8 +166,9 @@ def index():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             Download_File('./Uploads/'+filename)
             return redirect(url_for('req_zip', filename='songs.zip'))
+            # return render_template('index.html')
         if song.isspace() == True or song == '':
-            print('Blank')
+            pass
     return render_template('index.html')
 
 @app.route('/downloads/<filename>')
@@ -162,3 +182,9 @@ def req_zip(filename):
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
+    # a = input('1. Song Or 2. File')
+    # if a == '1':
+    #     b = input('Song Name:')
+    #     Download_Song(b)
+    # elif a == '2':
+    #     Download_File('./Uploads/songs.txt')
